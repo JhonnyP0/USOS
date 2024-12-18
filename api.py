cc72efdcf2751f1079ab61b92101eb6ddf4ffa2a
Jan <jaspachulski@gmail.com>
2024-12-18 15:49:39 +0100

    templates added

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
 import mysql.connector
 from dotenv import load_dotenv
 import os
@@ -20,9 +20,39 @@ db_config={
 def db_conn():
     return mysql.connector.connect(**db_config)
 
-@app.route('/', methods=['GET'])
-def get_users():
-    data=request.get_json()
-    
+@app.route('/')
+def login_page():
+    return render_template("loginpage.html")
+
+@app.route('/submit', methods=['POST'])
+def login_data_compre():
+    login=request.form.get('login')
+    password=request.form.get('password')
+
+    connection = db_conn()
+    cursor= connection.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE name = %s", (login,))
+    user=cursor.fetchone()
+
+    cursor.close()
+    connection.close()
+
+    if user:
+        db_pass=user[2]
+        if db_pass == password:
+            print("loged in")
+            return redirect(url_for('login_page')) # next template
+        else:
+            print("wrong password")
+            return redirect(url_for('login_page'))
+    else:
+        print("user not found")
+        return redirect(url_for('login_page'))
+
+@app.route('/welocome')
+def welcome():
+    return render_template("qwe.html")
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050, debug=True)
